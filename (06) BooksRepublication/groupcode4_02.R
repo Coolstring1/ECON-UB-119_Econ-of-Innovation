708cf2ee8bc0c498ca03370f3bf06e815d3b36c0
Chris Ren <daming112@gmail.com>
2020-11-30 20:39:41 -0500

    recent tweaking and outputs

--- (06) BooksRepublication/groupcode4_02.R
+++ (06) BooksRepublication/groupcode4_02.R
@@ -16,8 +16,11 @@ rm(list = ls())
 books <- read_stata("books_all.dta")
 
 
-###Question 1
 
+###Question 1###
+
+## Question 1A ##
+# summarizing the data
 books_summary <- books %>% 
   # this will only keep the first observation of each book (books are indexed by id). 
   # The exact year depends on how the data is sorted
@@ -25,44 +28,50 @@ books_summary <- books %>%
   group_by(field, brp) %>%
   summarize(number_books = n()) %>%
   mutate(brp = recode(brp,  `0` = "non_BRP", `1` = "BRP"))
-
 books_summary1 <- books_summary %>%
   group_by(brp)%>%
   summarise(total = sum(number_books))
-### Answer: 291 and compounds
+# Answer: 291 different books were subjected to the BRP and the subject of compounds was most frequently licensed
 
-##histograms
+## Question 1B ##
+# filtering books underneath threshold of 200 citations per book of its lifetime
 his <- books %>%
   filter(brp==1)%>%
   group_by(id)%>%
-  summarise(total_cite = sum(cit_year))
-
-
-his %>%
-  ggplot( aes(x=total_cite)) +
-  geom_histogram( binwidth=5, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
-  ggtitle("Bin size = 5") +
-  theme(
-    plot.title = element_text(size=15)
-  )
-###cutoff greater than 200 citations and make a note in write up 
+  summarise(total_cite = sum(cit_year)) %>%
+  filter(total_cite < 200)
 his1 <- books %>%
   filter(brp==1, year_c >1942)%>%
   group_by(id)%>%
-  summarise(total_cite = sum(cit_year))
-
-his1 %>%
+  summarise(total_cite = sum(cit_year)) %>%
+  filter(total_cite < 200)
+# histogram of citation count of books across the entire time period
+his %>%
   ggplot( aes(x=total_cite)) +
-  geom_histogram( binwidth=5, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
-  ggtitle("Bin size = 5") +
+  geom_histogram( binwidth=2, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  ggtitle("Citation Amounts Among BRP Books", subtitle = "(Bin width of 2, outliers above 200 citations excluded)") +
+  labs(x = "Citations", y = "Books with given number of citations") +
   theme(
     plot.title = element_text(size=15)
   )
+ggsave(paste0("histogram.png"),
+       width = 8, height = 5, units = "in")
+# histogram of citation count of books beyond the BRP year of 1942
+his1 %>%
+  ggplot( aes(x=total_cite)) +
+  geom_histogram( binwidth=2, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  ggtitle("Citation Among BRP Books After 1942", subtitle = "(Bin width of 2, outliers above 200 citations excluded)") +
+  labs(x = "Citations", y = "Books with given number of citations") +
+  theme(
+    plot.title = element_text(size=15)
+  )
+ggsave(paste0("histogram_post1942.png"),
+       width = 8, height = 5, units = "in")
 
 
-###Question 2
+###Question 2###
 
-##Comparing Citations to the same BRP Book by English-language and Other Authors
+## Addressing Question 2's issue 1: comparing Citations to the same BRP Book by English-language and Other Authors
 regr_table <- books %>%
   filter(brp == 1)%>%
   group_by(year_c,id) %>%
@@ -74,168 +83,212 @@ reg_table1<- regr_table %>%
   summarise(Eng = sum(Eng)/n(),
             Other = sum(Other)/n()) %>%
   filter(year_c>=1930) 
-  
-
+# plotting diff-in-diff
 ggplot(reg_table1) +
-  geom_line(aes(x=year_c, y=Eng, colour = "royalblue"), size = .8) +
-  geom_point(aes(x=year_c, y=Eng, colour = "royalblue"), size = 1.2) +
-  geom_line(aes(x=year_c, y=Other, colour = "darkorange2"), size = .8) +
-  geom_point(aes(x=year_c, y=Other, colour = "darkorange2"), size = 1.2) +
+  geom_line(aes(x=year_c, y=Eng, colour = "darkorange2"), size = .8) +
+  geom_point(aes(x=year_c, y=Eng, colour = "darkorange2"), size = 1.2) +
+  geom_line(aes(x=year_c, y=Other, colour = "royalblue"), size = .8) +
+  geom_point(aes(x=year_c, y=Other, colour = "royalblue"), size = 1.2) +
   geom_vline(xintercept = 1942, linetype = "dashed", size = 0.75) +
-  geom_text(aes(x=1938, label="BRP", y=0.8), 
+  geom_text(aes(x=1936, label="Introduction of BRP", y=0.8), 
             colour="black", 
             angle=0, 
             text=element_text(size=10)) +
   scale_color_identity(name = "", 
-                       breaks = c("royalblue", "darkorange2"), 
-                       labels = c("English", 
-                                  "Other"), 
+                       breaks = c("darkorange2", "royalblue"), 
+                       labels = c("English Citations", 
+                                  "Non-English Citations"), 
                        guide = "legend") +
   theme_bw() +
   xlab("Year") +
-  ylab("Citations per book and year") +
-  ggtitle("Figure 1 - Citation BRP Books in English verses Other languages") +
+  ylab("Citations per book") +
+  ggtitle("Citations per BRP Book in English vs Non-English Languages") +
   theme(legend.position = "right", legend.text = element_text(size = 10))
-
-
+ggsave(paste0("diff_in_diff_citations.png"),
+       width = 8, height = 5, units = "in")
+# quantifying diff-in-diff with regression
 reg_ass1 <- regr_table %>%
   filter(year_c >=1930) %>%
   mutate(change = Eng - Other) 
-
 reg_ass1$post <- ifelse(reg_ass1$year_c>=1942,1,0)
 reg_ass1$English <- ifelse(reg_ass1$Eng>0,1,0)
 reg_ass1$English_post <- ifelse(reg_ass1$English + reg_ass1$post>=2,1,0)
-
-reg_assumption1 <- felm(change ~ English_post| 
+reg_assumption1 <- felm(change ~ post + English + English_post| 
                id +year_c| # two FEs? use a + to add them
                0,
              data=reg_ass1) 
 summary(reg_assumption1)
+# write table to document
+huxreg(reg_assumption1,
+       stars = c(`*` = 0.1, `**` = 0.05, `***` = 0.01),
+       coefs = c("Treated X post-1942" = "English_post", # rename vars to smthing pretty
+                 "Treated" = "English"),
+       statistics = c("N" = "nobs",
+                      "R^2" = "r.squared")) %>%
+  add_rows(rbind(c("Year FE", "yes"), #note! you need more "yes" if you have >3 models
+                 c("State FE", "yes")),
+           copy_cell_props = FALSE,
+           after = c(nrow(.) - 3)) %>% quick_docx(file="report_reg1.docx")
+# note: observations counts every entry, but really the N of this sample was 291 unique books
 
+###Question 3 (Addressing Question 2's issue 2: Comparing BRP and Swiss books)###
 
-
-##Comparing BRP and Swiss books
-
+## Question 3a
+# filtering to define a comparable books subset
 fields_nonBRP <- books_summary %>%
   spread(brp, number_books) %>%#the new reshape that you should learn
-  filter(non_BRP>=10) %>%
+  filter(non_BRP>=2) %>%
   mutate(indic_non_brp_field = 1) # you will see why I created this
-
-# we now have 27 fields that have at lest 2 non-BRP books in them. Merge
-# to the original dataset
+# we now have 27 fields that have at lest 2 non-BRP books in them. Merge  to the original dataset
 books <- merge(books, fields_nonBRP,
                by = c("field"),
                all.x = TRUE)
+books_comparable <- books %>% filter(indic_non_brp_field ==1) %>%
+  filter(chemistry==1|mathematics==1) %>%
+  filter(!diss)
+# Answer: Chemistry and Math were subjects that were licensed heavily since Germany led those fields,
+# many German authors published in the Swiss copyright system which would have exempted some of their
+# work of comparably high quality from the BRP. Since chemistry and mathematics are well represented
+# in both the BRP set and the Swiss set and we know that many German authors published under the Swiss
+# copyright system, we focused on those two fields in particular. We also dropped dissertations since 
+# their behavior was quite different from an average science book
 
-books_comparable <- books %>% filter(indic_non_brp_field ==1)
-
-books_comparable <- books_comparable %>%
-  filter(chemistry==1|mathematics==1)
-
-#################
-##try to calculate similar number of citations here but something went wrong
-test <- books_comparable %>% 
-  group_by(id,brp)%>%
-  summarise(total_cite = sum(cit_year))
-
-
-testw <- reshape(as.data.frame(test),
-                         idvar = c("id"), # which var is id
-                         direction = "wide", # how you want things reshaped
-                         timevar = "brp",  # which var is repeating
-                         sep = "_") 
-
-testw <- testw %>%
-  mutate(ch = total_cite_1-total_cite_0)
-###################
-
+## Question 3b
+# reshaping for diff=in-diff
 regr_table2 <- books_comparable %>%
   group_by(year_c,brp,id)%>%
   summarise(Eng = sum(count_eng))
-
-test <- regr_table2 %>%
-  group_by(brp)%>%
-  summarise(n = n())
-
 regr_table2_1 <- regr_table2 %>%
   group_by(year_c,brp) %>%
   summarise(cit = sum(Eng)/n())
-
-
 regr_table2_w <- reshape(as.data.frame(regr_table2_1),
                      idvar = c("year_c"), # which var is id
                      direction = "wide", # how you want things reshaped
                      timevar = "brp",  # which var is repeating
                      sep = "_") %>%
   filter(year_c>=1930)
-
-
-
+# plotting diff-in-diff
 ggplot(regr_table2_w) +
   geom_line(aes(x=year_c, y=cit_0, colour = "royalblue"), size = .8) +
   geom_point(aes(x=year_c, y=cit_0, colour = "royalblue"), size = 1.2) +
   geom_line(aes(x=year_c, y=cit_1, colour = "darkorange2"), size = .8) +
   geom_point(aes(x=year_c, y=cit_1, colour = "darkorange2"), size = 1.2) +
   geom_vline(xintercept = 1942, linetype = "dashed", size = 0.75) +
-  geom_text(aes(x=1938, label="BRP", y=0.8), 
+  geom_text(aes(x=1936, label="Introduction of BRP", y=0.8), 
             colour="black", 
             angle=0, 
             text=element_text(size=10)) +
   scale_color_identity(name = "", 
-                       breaks = c("royalblue", "darkorange2"), 
-                       labels = c("Swiss", 
-                                  "BRP"), 
+                       breaks = c("darkorange2","royalblue"), 
+                       labels = c("BRP Licensed Books",
+                                  "Swiss Books"), 
                        guide = "legend") +
   theme_bw() +
   xlab("Year") +
-  ylab("Citations per book and year") +
-  ggtitle("Figure 2 - Citation BRP Books and Swiss Book") +
+  ylab("Citations per book") +
+  ggtitle("Citations per Book for BRP Licensed Books vs Swiss Book") +
   theme(legend.position = "right", legend.text = element_text(size = 10))
+ggsave(paste0("diff_in_diff_swiss.png"),
+       width = 8, height = 5, units = "in")
 
-####regression
-
+## Question 3c
+# quantifying diff-in-diff with regression
 reg_ass2 <- regr_table2
 reg_ass2$post <- ifelse(reg_ass2$year_c>=1942,1,0)
 reg_ass2$post_treated <- ifelse(reg_ass2$post + reg_ass2$brp>=2,1,0)
-
 reg_assumption2 <- felm(Eng ~ post_treated| 
                           id +year_c| # two FEs? use a + to add them
                           0,
                         data=reg_ass2) 
 summary(reg_assumption2)
+# write table to document
+huxreg(reg_assumption2,
+       stars = c(`*` = 0.1, `**` = 0.05, `***` = 0.01),
+       coefs = c("Treated X post-1942" = "post_treated"),
+       statistics = c("N" = "nobs",
+                      "R^2" = "r.squared")) %>%
+  add_rows(rbind(c("Year FE", "yes"), #note! you need more "yes" if you have >3 models
+                 c("State FE", "yes")),
+           copy_cell_props = FALSE,
+           after = c(nrow(.) - 3)) %>%  quick_docx(file = "report_reg2.docx")
 
 
-
-##distance
-knn <- books %>%
-  group_by(id,field,year_c) %>%
-  summarise(cite = sum(count_eng) + sum(count_noeng))
-
-knn1 <-knn  %>%
-  filter(year_c<1942) %>%
-  group_by(field,id) %>%
-  summarise(cite = sum(cite))
-knn1 <- knn1%>%
-  filter(field!='')
-
-knn1 <- knn1 %>% 
-  mutate(v = 1, fie = field) %>% 
-  spread(fie, v, fill = 0)
-
-normalize <- function(x){
-  return ((x-min(x)) / (max(x)-min(x)))
-}
-sub <- knn1[,3:36]
-subset <- as.data.frame(lapply(knn1[,3:36],normalize))
-set.seed(0)
-temp <- as.data.frame(mahalanobis(sub, colMeans(sub), cov(sub),tol=1e-20))
-final <- cbind(knn1,temp)
-names(final)[37] <- "distance"
-
-set <- final %>%
-  select('id','distance') %>%
-  filter(distance <= 2)
-book_wanted <- set[2]
-
+# ## Question 3d
+# # filtering and reshapping for input into mahalanobis function
+# knn <- books %>%
+#   group_by(id,field,year_c) %>%
+#   summarise(cite = sum(count_eng) + sum(count_noeng))
+# knn1 <-knn  %>%
+#   filter(year_c<1942) %>%
+#   group_by(field,id) %>%
+#   summarise(cite = sum(cite))
+# knn1 <- knn1%>%
+#   filter(field!='')
+# knn1 <- knn1 %>% 
+#   mutate(v = 1, fie = field) %>% 
+#   spread(fie, v, fill = 0)
+# # creation of normalize function
+# normalize <- function(x){
+#   return ((x-min(x)) / (max(x)-min(x)))
+# }
+# # subset data and applying normalize
+# sub <- knn1[,3:36]
+# subset <- as.data.frame(lapply(knn1[,3:36],normalize))
+# set.seed(0)
+# # input of mahalanobis function results and binding to original dataframe
+# temp <- as.data.frame(sqrt(mahalanobis(sub, colMeans(sub), cov(sub),tol=1e-20)))
+# final <- cbind(knn1,temp)
+# names(final)[37] <- "distance"
+# # filtering of distances under threshold of closeness
+# set <- final %>%
+#   select('id','distance') %>%
+#   filter(distance <= 1)
+# book_wanted <- set[2]
+# 
+# # reshaping for diff=in-diff
+# regr_table3 <- books_comparable %>%
+#   filter(books_comparable$id %in% book_wanted$id) %>%
+#   group_by(year_c,brp,id)%>%
+#   summarise(Eng = sum(count_eng))
+# regr_table3_1 <- regr_table3 %>%
+#   group_by(year_c,brp) %>%
+#   summarise(cit = sum(Eng)/n())
+# regr_table3_w <- reshape(as.data.frame(regr_table3_1),
+#                          idvar = c("year_c"), # which var is id
+#                          direction = "wide", # how you want things reshaped
+#                          timevar = "brp",  # which var is repeating
+#                          sep = "_") %>%
+#   filter(year_c>=1930)
+# # plotting diff-in-diff
+# ggplot(regr_table3_w) +
+#   geom_line(aes(x=year_c, y=cit_0, colour = "royalblue"), size = .8) +
+#   geom_point(aes(x=year_c, y=cit_0, colour = "royalblue"), size = 1.2) +
+#   geom_line(aes(x=year_c, y=cit_1, colour = "darkorange2"), size = .8) +
+#   geom_point(aes(x=year_c, y=cit_1, colour = "darkorange2"), size = 1.2) +
+#   geom_vline(xintercept = 1942, linetype = "dashed", size = 0.75) +
+#   geom_text(aes(x=1938, label="BRP", y=0.8), 
+#             colour="black", 
+#             angle=0, 
+#             text=element_text(size=10)) +
+#   scale_color_identity(name = "", 
+#                        breaks = c("darkorange2","royalblue"), 
+#                        labels = c("BRP",
+#                                   "Swiss"), 
+#                        guide = "legend") +
+#   theme_bw() +
+#   xlab("Year") +
+#   ylab("Citations per book and year") +
+#   ggtitle("Figure 2 - Citation BRP Books and Swiss Book") +
+#   theme(legend.position = "right", legend.text = element_text(size = 10))
+# 
+# # quantifying diff-in-diff with regression
+#  reg_ass3 <- regr_table3
+# reg_ass3$post <- ifelse(reg_ass3$year_c>=1942,1,0)
+# reg_ass3$post_treated <- ifelse(reg_ass3$post + reg_ass3$brp>=2,1,0)
+# reg_assumption3 <- felm(Eng ~ post_treated| 
+#                           id +year_c| # two FEs? use a + to add them
+#                           0,
+#                         data=reg_ass3) 
+# summary(reg_assumption3)
 
 
